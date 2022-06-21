b12a0d8fe2527c97b8bc16bac552469b8a4d2053
Tom Dyas <tdyas@toolchain.com>
2022-06-21 14:16:25 -0400

    fmt

--- src/jvm/org/pantsbuild/example/app/ExampleApp.kt
+++ src/jvm/org/pantsbuild/example/app/ExampleApp.kt
@@ -3,5 +3,5 @@ package org.pantsbuild.example.app
 import org.pantsbuild.example.lib.ExampleLib
 
 fun main(args: Array<String>) {
-  println(ExampleLib.hello())
+    println(ExampleLib.hello())
 }
--- tests/jvm/org/pantsbuild/example/lib/ExampleLibTest.kt
+++ tests/jvm/org/pantsbuild/example/lib/ExampleLibTest.kt
@@ -1,14 +1,12 @@
 package org.pantsbuild.example
 
+import org.pantsbuild.example.lib.ExampleLib
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-import org.pantsbuild.example.lib.ExampleLib
-
 internal class ExampleLibTest {
-  @Test
-  fun testShouldSayHello() {
-    assertEquals(ExampleLib.hello(), "Hello World!")
-  }
+    @Test
+    fun testShouldSayHello() {
+        assertEquals(ExampleLib.hello(), "Hello World!")
+    }
 }
-
